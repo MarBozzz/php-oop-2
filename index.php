5eacd07b522b3853595778aa80ce06e7f8eb4029
MarBozzz <mbozzuto@libero.it>
2022-12-12 18:42:34 +0100

    [added] first printed array

--- index.php
+++ index.php
@@ -11,11 +11,32 @@ require_once __DIR__ . '/models/Toy.php';
 require_once __DIR__ . '/models/Accessory.php';
 
 $crocchini = new Food(new Category('Cat', 'catIcon'), 'Croccantini', 'Felix', 'Pepite', 101403, 6.99, true, './assets/crocchini.jpg', 950, 'Crunchy and soft', 'Delicious');
+$petFood = new Food(new Category('Dog', 'dogIcon'), 'Cibo secco', 'Monge', 'Superpemium', 100558, 57.99, true, './assets/agnello.jpg', 3950, 'Agnello & Riso', 'Agnello');
 
-var_dump($crocchini);
-var_dump($crocchini->category->name);
+$cuccia = new Accessory(new Category('Dog', 'dogIcon'), 'Cuccia', 'Trixie', ' Natura Classic', 91034, 42.90, true, './assets/cuccia.jpg','plastic', '80x70x120');
+$tiragraffi = new Accessory(new Category('Cat', 'catIcon'), 'Tiragraffi', 'Natural Paradise Orchid', 'L', 15200, 152.90, false, './assets/tiragraffi.jpg','wood', '40x60x100');
 
+$palla = new Toy(new Category('Dog', 'dogIcon'), 'Chuckit!', 'Chuckit!', 'Chuckit!', 25228, 3.99, false, './assets/palla.jpg', 'caucciu', '10x10');
+$pelouche = new Toy(new Category('Cat', 'catIcon'), 'KONG', 'Shells Turtle!', 'Turtle!', 78228, 9.99, true, './assets/turtle.jpg', 'nylon e TPR', '25x10');
+
+$foods = [
+  new Food(new Category('Cat', 'catIcon'), 'Croccantini', 'Felix', 'Pepite', 101403, 6.99, true, './assets/crocchini.jpg', 950, 'Crunchy and soft', 'Delicious'),
+  $petFood = new Food(new Category('Dog', 'dogIcon'), 'Cibo secco', 'Monge', 'Superpemium', 100558, 57.99, true, './assets/agnello.jpg', 3950, 'Agnello & Riso', 'Agnello')
+];
+
+$accessories = [
+  new Accessory(new Category('Dog', 'dogIcon'), 'Cuccia', 'Trixie', ' Natura Classic', 91034, 42.90, true, './assets/cuccia.jpg','plastic', '80x70x120'),
+  new Accessory(new Category('Cat', 'catIcon'), 'Tiragraffi', 'Natural Paradise Orchid', 'L', 15200, 152.90, false, './assets/tiragraffi.jpg','wood', '40x60x100')
+];
+
+$toys = [
+  new Toy(new Category('Dog', 'dogIcon'), 'Chuckit!', 'Chuckit!', 'Chuckit!', 25228, 3.99, false, './assets/palla.jpg', 'caucciu', '10x10'),
+  new Toy(new Category('Cat', 'catIcon'), 'KONG', 'Shells Turtle!', 'Turtle!', 78228, 9.99, true, './assets/turtle.jpg', 'nylon e TPR', '25x10'),
+  new Toy(new Category('Dog', 'dogIcon'), 'Chuckit!', 'Chuckit!', 'Chuckit!', 25228, 3.99, false, './assets/palla.jpg', 'caucciu', '10x10'),
+  new Toy(new Category('Cat', 'catIcon'), 'KONG', 'Shells Turtle!', 'Turtle!', 78228, 9.99, true, './assets/turtle.jpg', 'nylon e TPR', '25x10')
+]
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -23,9 +44,47 @@ var_dump($crocchini->category->name);
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+  <style>
+    img {
+      max-width: 100%;
+    }
+    .red {
+      color: red
+    }
+  </style>
   <title>Pet E-Shop</title>
 </head>
 <body>
+
+<div class="container">
+  <div class="row py-5 text-center">
+
+    <h2 class="p-5">TOYS</h2>
+    <?php foreach($toys as $toy): ?>
+    <div class="col-3 mb-5">
+      <div class="card" >
+        <img src="<?php echo $toy->image ?>" class="card-img-top" alt="<?php echo $toy->name ?>">
+        <div class="card-body">
+          <h3 class="card-title py-3"><?php echo 'Product: <br/>' . '<strong>' . $toy->name . '</strong>'?></h3>
+          <h4 class="card-title"><?php echo 'Brand: ' .$toy->brand ?></h4>
+          <h4 class="card-title"><?php echo 'Model: ' .$toy->model ?></h4>
+          <h5 class="card-title"><?php echo 'Feature: ' .$toy->feature ?></h5>
+          <h5 class="card-title"><?php echo 'Size: ' .$toy->size ?></h5>
+          <h5 class="card-title"><?php echo 'Price: ' .$toy->price ?></h5>
+          <?php if ($toy->isAvailable) {
+             echo '<h5 class="card-title"> Product Available </h5>';
+            } else {
+            echo '<h5 class="card-title red"> Product Not Available </h5>';
+            };
+           ?>
+        </div>
+      </div>
+    </div>
+
+    <?php endforeach; ?>
+  </div>
+</div>
   
 </body>
 </html>
